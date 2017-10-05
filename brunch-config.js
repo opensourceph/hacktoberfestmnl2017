04491db80be30dc2010e1dbe46b83d224de8299f
jekku <jekripreclaroorlina@gmail.com>
2017-10-05 11:24:27 +0800

    Integrate brunch with Google Materials

--- brunch-config.js
+++ brunch-config.js
@@ -1,5 +1,16 @@
 // See http://brunch.io for documentation.
-exports.files = {
-  javascripts: {joinTo: 'app.js'},
-  stylesheets: {joinTo: 'app.css'}
-};
+module.exports = {
+  npm: {
+    styles: {
+      'material-components-web': ['dist/material-components-web.min.css']
+    }
+  },
+  files: {
+    javascripts: {
+      joinTo: 'app.js'
+    },
+    stylesheets: {
+      joinTo: 'app.css'
+    }
+  }
+}
